642b64b24b0fc0f43d2c28065f1145315d544751
ItsTushar16 <itstiwaribhaiya16@gmail.com>
2026-01-02 01:24:56 +0530

    Initial commit

---
+++ models/listing.js
@@ -0,0 +1,55 @@
+// const mongoose = require("mongoose");
+// const Schema= mongoose.Schema;
+
+// const listingSchema= new Schema({
+//     title: {
+//         type:String,
+//         required:true,
+//     },    
+//     description:String,
+//     image:{
+//         default:"https://images.unsplash.com/photo-1696393800648-8281a3fc21d7?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//         type:String,
+//         set:(v) => v === "" ? "https://images.unsplash.com/photo-1696393800648-8281a3fc21d7?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D ":v, 
+//     },    
+//     price:Number,
+//     location:String,
+//     country:String,
+
+// });
+
+// const Listing = mongoose.model("Listing" , listingSchema);
+// module.exports= Listing;
+
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    // filename: {
+    //   type: String,
+    //   default: "listingimage",
+    // },
+    url: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1696393800648-8281a3fc21d7?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0",
+      set: (v) =>
+        v === ""
+          ? "https://images.unsplash.com/photo-1696393800648-8281a3fc21d7?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0"
+          : v,
+    },
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
